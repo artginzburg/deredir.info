f84f66706f820499cc8d9333d25caa174e0cbff1
Art Ginzburg <ginzburg.arthur@gmail.com>
2023-10-02 23:48:23 +0200

    Start remaking the old markup

--- detect-redirect/src/app/page.tsx
+++ detect-redirect/src/app/page.tsx
@@ -3,10 +3,13 @@ import { getRedirects } from './actions';
 export default function Home() {
   return (
     <main>
-      Hello
+      <h1>
+        <span>Detect</span> Redirect
+      </h1>
+      <p>Unshorten and trace URLs</p>
       <form action={getRedirects}>
-        <input placeholder="Website address" name="link" />
-        <button>Trace</button>
+        <input placeholder="somesite.com/posts" name="link" />
+        <button type="submit">Trace</button>
       </form>
     </main>
   );
