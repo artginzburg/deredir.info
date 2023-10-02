01c7b64d0126ab356f18aa32d71aa20b857999e4
Art Ginzburg <ginzburg.arthur@gmail.com>
2023-10-02 23:37:14 +0200

    Finish logic for getRedirects()

--- detect-redirect/src/app/actions.tsx
+++ detect-redirect/src/app/actions.tsx
@@ -1,15 +1,10 @@
 'use server';
 
-import { headers } from 'next/headers';
-
 export async function getRedirects(formData: FormData) {
   const link = formData.get('link');
   if (!link || typeof link !== 'string') return 'Could not process';
 
   // const requestHeaders = headers();
-  // console.log(requestHeaders.get('User-agent'));
-  // console.log(requestHeaders.get('Cookie'));
-
   // const exposedUserHeaders = {
   //   'User-agent': requestHeaders.get('User-agent') ?? undefined,
   //   Cookie: requestHeaders.get('Cookie') ?? undefined,
@@ -21,21 +16,22 @@ export async function getRedirects(formData: FormData) {
   const totalDuration = recursiveResult.reduce((prev, cur) => prev + cur.duration, 0);
   console.log('totalDuration', totalDuration);
 
-  const directLinkFasterByTimes = totalDuration / (recursiveResult.at(-1)?.duration ?? 0);
+  const directLinkFasterByTimes = totalDuration / (recursiveResult.at(-1)?.duration ?? 1);
   console.log('directLinkFasterByTimes', directLinkFasterByTimes);
 }
 
-const fetchWithAddProtocolAndRetry: typeof fetch = async (input, init) => {
-  // TODO if no protocol — try https://, then try http://, then fail. Use nested trycatch if necessary.
+const fetchWithProtocolRetry: typeof fetch = async (input, init) => {
   try {
     return await fetch(input, init);
   } catch (error) {
     const foundProtocols = String(input).search(/http(s):\/\//);
     const hasProtocol = foundProtocols !== -1;
     if (hasProtocol) throw error;
-    const protocolToSet = String(input).startsWith('https://') ? 'http://' : 'https://';
-    // console.log('protocolToSet', protocolToSet);
-    return await fetch(`${protocolToSet}${input}`);
+    try {
+      return await fetch(`https://${input}`, init);
+    } catch (error) {
+      return await fetch(`http://${input}`, init);
+    }
   }
 };
 
@@ -45,63 +41,67 @@ type RecursiveFetchState = {
   statusText: string;
   nextLink: string | null;
   duration: number;
+  errorCode?: string;
 };
 
 async function recursiveFetchWithManualRedirect(
   link: string,
   collectedData: RecursiveFetchState[] = [],
   headers?: Record<string, string | undefined>,
-) {
+): Promise<RecursiveFetchState[]> {
   const startTime = performance.now();
+  const usernameForFakeHeaders = 'ginzart';
   const fetchHeaders = {
-    // "Accept-language: en\r\n" .
-    // "Cookie: fqtr=iam\r\n" .
-    // ($_GET['context'] === 'my') ? "User-agent: ".$_SERVER['HTTP_USER_AGENT'] : "User-agent: Safari Google fqtr\r\n"
-
     'Accept-language': 'en',
-    Cookie: 'ginzart=iam',
-    'User-agent': 'Safari Google ginzart',
+    Cookie: `${usernameForFakeHeaders}=iam`,
+    'User-agent': `Safari Google ${usernameForFakeHeaders}`,
     ...headers,
   };
-  // console.log('fetchHeaders', fetchHeaders);
-  const response = await fetchWithAddProtocolAndRetry(link, {
-    method: 'GET',
-    redirect: 'manual',
-    mode: 'no-cors',
-    cache: 'no-cache',
-    headers: fetchHeaders,
-  });
-  const endTime = performance.now();
-  const duration = endTime - startTime;
 
-  // const currentLink = response.url;
-  // const currentStatus = response.status;
-  // const currentStatusText = response.statusText;
-  const nextLink = response.headers.get('location');
+  try {
+    const response = await fetchWithProtocolRetry(link, {
+      method: 'GET',
+      redirect: 'manual',
+      mode: 'no-cors',
+      cache: 'no-cache',
+      headers: fetchHeaders,
+    });
+    const endTime = performance.now();
+    const duration = endTime - startTime;
 
-  // const stateSymbol = Object.getOwnPropertySymbols(response).find(
-  //   (symbol) => symbol.description === 'state',
-  // );
-  // // @ts-expect-error
-  // const state = response[stateSymbol];
-  // if (!state) return 'No response state found';
+    const nextLink = response.headers.get('location');
 
-  // console.log('state', state);
-  // const { timingInfo } = state;
-  // const duration = timingInfo === null ? null : timingInfo.endTime - timingInfo.startTime;
+    const newState: RecursiveFetchState = {
+      link: response.url,
+      status: response.status,
+      statusText: response.statusText,
+      nextLink,
+      duration,
+    };
+    const preservedData: RecursiveFetchState[] = [...collectedData, newState];
 
-  const newState = {
-    link: response.url,
-    status: response.status,
-    statusText: response.statusText,
-    nextLink,
-    duration,
-  };
-  const preservedData = [...collectedData, newState];
+    if (nextLink) {
+      return recursiveFetchWithManualRedirect(nextLink, preservedData);
+    } else {
+      return preservedData;
+    }
+  } catch (error) {
+    const errorState: RecursiveFetchState = {
+      link,
+      status: 0,
+      statusText: '',
+      nextLink: null,
+      duration: 0,
+      errorCode:
+        error instanceof Error &&
+        error.cause &&
+        typeof error.cause === 'object' &&
+        'code' in error.cause &&
+        typeof error.cause.code === 'string'
+          ? error.cause.code
+          : 'Unknown error',
+    };
 
-  if (nextLink) {
-    return recursiveFetchWithManualRedirect(nextLink, preservedData);
-  } else {
-    return preservedData;
+    return [...collectedData, errorState];
   }
 }
