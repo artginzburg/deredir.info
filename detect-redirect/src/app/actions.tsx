7cd493025f1f6d290b2288c807588d2741b49a35
Art Ginzburg <ginzburg.arthur@gmail.com>
2023-10-02 18:45:22 +0200

    More initial logic replicated

--- detect-redirect/src/app/actions.tsx
+++ detect-redirect/src/app/actions.tsx
@@ -1,76 +1,94 @@
 'use server';
 
+import { headers } from 'next/headers';
+
 export async function getRedirects(formData: FormData) {
   const link = formData.get('link');
   if (!link || typeof link !== 'string') return 'Could not process';
 
-  const recursiveResult = await recursiveFetchWithManualRedirect(link);
+  // const requestHeaders = headers();
+  // console.log(requestHeaders.get('User-agent'));
+  // console.log(requestHeaders.get('Cookie'));
+
+  // const exposedUserHeaders = {
+  //   'User-agent': requestHeaders.get('User-agent') ?? undefined,
+  //   Cookie: requestHeaders.get('Cookie') ?? undefined,
+  // }
+
+  const recursiveResult = await recursiveFetchWithManualRedirect(link, undefined);
   console.log('recursiveResult', recursiveResult);
 
-  const response = await fetch(link, {
-    method: 'HEAD',
-    // redirect: 'manual',
-    redirect: 'manual',
-  });
-  const currentLink = response.url;
-  const currentStatus = response.status;
-  const currentStatusText = response.statusText;
-  const nextLink = response.headers.get('location');
+  const totalDuration = recursiveResult.reduce((prev, cur) => prev + cur.duration, 0);
+  console.log('totalDuration', totalDuration);
 
-  const stateSymbol = Object.getOwnPropertySymbols(response).find(
-    (symbol) => symbol.description === 'state',
-  );
-  // @ts-expect-error
-  const state = response[stateSymbol];
-  if (!state) return 'No response state found';
-
-  console.log('state', state);
-  const { timingInfo } = state;
-  const duration = timingInfo.endTime - timingInfo.startTime;
-  console.log('request duration', duration);
+  const directLinkFasterByTimes = totalDuration / (recursiveResult.at(-1)?.duration ?? 0);
+  console.log('directLinkFasterByTimes', directLinkFasterByTimes);
 }
+
+const fetchWithAddProtocolAndRetry: typeof fetch = async (input, init) => {
+  // TODO if no protocol — try https://, then try http://, then fail. Use nested trycatch if necessary.
+  try {
+    return await fetch(input, init);
+  } catch (error) {
+    const foundProtocols = String(input).search(/http(s):\/\//);
+    const hasProtocol = foundProtocols !== -1;
+    if (hasProtocol) throw error;
+    const protocolToSet = String(input).startsWith('https://') ? 'http://' : 'https://';
+    // console.log('protocolToSet', protocolToSet);
+    return await fetch(`${protocolToSet}${input}`);
+  }
+};
 
 type RecursiveFetchState = {
   link: string;
   status: number;
   statusText: string;
   nextLink: string | null;
-  duration: number | null;
+  duration: number;
 };
 
 async function recursiveFetchWithManualRedirect(
   link: string,
   collectedData: RecursiveFetchState[] = [],
+  headers?: Record<string, string | undefined>,
 ) {
-  const response = await fetch(link, {
-    method: 'HEAD',
-    // redirect: 'manual',
-    redirect: 'manual',
-    headers: {
-      // "Accept-language: en\r\n" .
-      // "Cookie: fqtr=iam\r\n" .
-      // ($_GET['context'] === 'my') ? "User-agent: ".$_SERVER['HTTP_USER_AGENT'] : "User-agent: Safari Google fqtr\r\n"
+  const startTime = performance.now();
+  const fetchHeaders = {
+    // "Accept-language: en\r\n" .
+    // "Cookie: fqtr=iam\r\n" .
+    // ($_GET['context'] === 'my') ? "User-agent: ".$_SERVER['HTTP_USER_AGENT'] : "User-agent: Safari Google fqtr\r\n"
 
-      'Accept-language': 'en',
-      Cookie: 'ginzart=iam',
-      'User-Agent': 'Safari Google ginzart',
-    },
+    'Accept-language': 'en',
+    Cookie: 'ginzart=iam',
+    'User-agent': 'Safari Google ginzart',
+    ...headers,
+  };
+  // console.log('fetchHeaders', fetchHeaders);
+  const response = await fetchWithAddProtocolAndRetry(link, {
+    method: 'GET',
+    redirect: 'manual',
+    mode: 'no-cors',
+    cache: 'no-cache',
+    headers: fetchHeaders,
   });
-  const currentLink = response.url;
-  const currentStatus = response.status;
-  const currentStatusText = response.statusText;
+  const endTime = performance.now();
+  const duration = endTime - startTime;
+
+  // const currentLink = response.url;
+  // const currentStatus = response.status;
+  // const currentStatusText = response.statusText;
   const nextLink = response.headers.get('location');
 
-  const stateSymbol = Object.getOwnPropertySymbols(response).find(
-    (symbol) => symbol.description === 'state',
-  );
-  // @ts-expect-error
-  const state = response[stateSymbol];
-  if (!state) return 'No response state found';
+  // const stateSymbol = Object.getOwnPropertySymbols(response).find(
+  //   (symbol) => symbol.description === 'state',
+  // );
+  // // @ts-expect-error
+  // const state = response[stateSymbol];
+  // if (!state) return 'No response state found';
 
-  console.log('state', state);
-  const { timingInfo } = state;
-  const duration = timingInfo === null ? null : timingInfo.endTime - timingInfo.startTime;
+  // console.log('state', state);
+  // const { timingInfo } = state;
+  // const duration = timingInfo === null ? null : timingInfo.endTime - timingInfo.startTime;
 
   const newState = {
     link: response.url,
